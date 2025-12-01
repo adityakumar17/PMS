55ab571d2e7838022c8a1478b0da991dc5659538
Aditya Kumar <adityakumar1722003@gmail.com>
2025-12-01 12:48:24 +0530

    Fix allowed hosts for Render deployment

--- Pharmacyproject/settings.py
+++ Pharmacyproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h8$m#+8%(7pqjxjna5yrydr%ijzxabnfn0gqnbcz)=pxnmqsq#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pms-6dwq.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
